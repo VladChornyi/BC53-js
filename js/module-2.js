71003f80309072bb12193923d1e1a35cda0b5eea
RomanSulimovskyi <mr.sulimovskiy@gmail.com>
2023-04-07 14:49:11 +0100

    2 chance

--- js/module-2.js
+++ js/module-2.js
@@ -1,5 +1,15 @@
 // 1.Перетворити рядок, що містить слова, розділені комами, в масив слів і вивести кожне слово в окремому рядку.
-// const str = "JavaScript, HTML, CSS, React";
+const str = "JavaScript, HTML, CSS, React";
+
+const strArr = str.split(', ')
+
+for(let i=0; i<strArr.length; i +=1) {
+console.log(strArr[i]);
+}
+
+
+
+
 
 // 2.Об'єднати масив слів в рядок, розділений комами та пробілами.
 // const words = ["JavaScript", "HTML", "CSS", "React"];
