2a62e1392c5ca4b7890b9f28a4ad27dbcb446404
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-04-10 20:48:08 +0300

    10 завдання 2 модуль

--- js/module-2.js
+++ js/module-2.js
@@ -75,19 +75,86 @@
 
 // 8. Написати функцію, яка приймає рядок і повертає кількість голосних літер у цьому рядку.
 // const vowels = ["a", "e", "i", "o", "u"];
-const str = 'This is a new line'
+// const str = 'This is a new line'
 
-function countVowels(string){
-    const vowels = ["a", "e", "i", "o", "u"];
-    let count = 0;
-    for (const element of string) {   
-        count += vowels.includes(element) ? 1 : 0;
+// function countVowels(string){
+//     const vowels = ["a", "e", "i", "o", "u"];
+//     let count = 0;
+//     for (const element of string) {
+//         count += vowels.includes(element) ? 1 : 0;
+//     }
+//     return `Містить ${count} голосних`;
+// }
+
+// console.log(str, countVowels(str))
+
+// 9. Написати функцію, яка приймає два параметри - рядок та символ - і повертає новий рядок,
+// в якому всі входження символу в початковому рядку замінені на пустий рядок.
+
+const newString = (message, simvol) => {
+  const arrMessags = message.split("");
+
+  for (let i = 0; i < arrMessags.length; i++) {
+    if (arrMessags[i] === simvol) {
+      arrMessags[i] = "\n";
     }
-    return `Містить ${count} голосних`;
-} 
+  }
+  console.log(arrMessags.join(" "));
+};
 
-console.log(str, countVowels(str))
+newString(
+  "Написати функцію, яка приймає два параметри - рядок та символ - і повертає новий рядок,в якому всі входження символу в початковому рядку замінені на пустий рядок",
+  "а"
+);
 
-// 9. Написати функцію, яка приймає два параметри - рядок та символ - і повертає новий рядок, в якому всі входження символу в початковому рядку замінені на пустий рядок.
+//10. Написати функцію, яка приймає масив чисел та повертає новий масив, який містить всі числа,
+// які є дублікатами в початковому масиві.Якщо немає дублікатів, функція повертає порожній масив.
 
-//10. Написати функцію, яка приймає масив чисел та повертає новий масив, який містить всі числа, які є дублікатами в початковому масиві. Якщо немає дублікатів, функція повертає порожній масив.
+const array = [
+  48, 1, 5, 25, 22, 10, 44, 102, 100, 22, 53, 6, 3, 10, 45, 12, 0, 22, 13, 22,
+  45, 55, 33, 23, 5, 21, 2, 44, 1,
+];
+//======================Метод 1=====================
+const noDublicatInt = (array) => {
+  const newArray = [];
+  //   for (let ind = 0; ind < array.length; ind += 1) {
+  //     const dubI = array[ind];
+  //     let count = 0;
+  //     for (let index = ind; index < array.length; index += 1) {
+  //       const el = array[index];
+  //       if (el === dubI) {
+  //         count += 1;
+  //         if (count === 2) {
+  //           !newArray.includes(el) && newArray.push(el);
+  //           continue;
+  //         }
+  //       }
+  //     }
+  //   }
+  //   console.log(newArray);
+  //   return newArray;
+  //======================Метод 2=====================
+  const newObj = {};
+
+  for (let i = 0; i < array.length; i += 1) {
+    let count = 1;
+    if (newObj[array[i]]) {
+      count += 1;
+      newObj[array[i]] = count;
+    } else {
+      newObj[array[i]] = count;
+    }
+  }
+
+  for (var i in newObj) {
+    let result;
+    if (newObj.hasOwnProperty(i)) {
+      newObj[i] === 2 && newArray.push(Number(i));
+    }
+  }
+
+  console.log(newObj);
+  console.log(newArray);
+};
+
+noDublicatInt(array);
