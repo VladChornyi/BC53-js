c0de52c135406730a4df47a1e58a58119feb7a70
ginkosoul <ginkosoul@gmail.com>
2023-04-10 15:59:03 +0300

    task number 8

--- js/module-2.js
+++ js/module-2.js
@@ -61,20 +61,32 @@
 
 // 7. Написати функцію, яка повертає найбільше число з масиву.
 
-const numbers = [5, 12, 3, 8, 7, 15, 11, 18];
-function maxNum(numbers) {
-  let max = numbers[0];
-for (const iterator of numbers) {
-  if (iterator > max) {
-    max = iterator
-  }
-}
-  return max
-}
-console.log(maxNum(numbers));
+// const numbers = [5, 12, 3, 8, 7, 15, 11, 18];
+// function maxNum(numbers) {
+//   let max = numbers[0];
+// for (const iterator of numbers) {
+//   if (iterator > max) {
+//     max = iterator
+//   }
+// }
+//   return max
+// }
+// console.log(maxNum(numbers));
 
 // 8. Написати функцію, яка приймає рядок і повертає кількість голосних літер у цьому рядку.
 // const vowels = ["a", "e", "i", "o", "u"];
+const str = 'This is a new line'
+
+function countVowels(string){
+    const vowels = ["a", "e", "i", "o", "u"];
+    let count = 0;
+    for (const element of string) {   
+        count += vowels.includes(element) ? 1 : 0;
+    }
+    return `Містить ${count} голосних`;
+} 
+
+console.log(str, countVowels(str))
 
 // 9. Написати функцію, яка приймає два параметри - рядок та символ - і повертає новий рядок, в якому всі входження символу в початковому рядку замінені на пустий рядок.
 
