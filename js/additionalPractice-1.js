d95840738ca0a88f3ce2086d4528fea79d76f649
Nikita <bazilik2452@gmail.com>
2023-04-11 14:39:42 +0200

    /

--- js/additionalPractice-1.js
+++ js/additionalPractice-1.js
@@ -24,24 +24,24 @@
 // }
 
 // 2-a. Напишіть функцію sumEvenAndOdd, яка приймає масив чисел та повертає масив з двома елементами  - сума всіх його парних елементів, та сума всіх непарних.
-const array = [1, 2, 3, 4, 5];
-const sum = sumEvenAndOdd(array);
-console.log(sum); // [6,9]
+// const array = [1, 2, 3, 4, 5];
+// const sum = sumEvenAndOdd(array);
+// console.log(sum); // [6,9]
 
-function sumEvenAndOdd(array) {
-  let sumEven = 0;
-  let sumOdd = 0;
-  for (let i = 1; i <= array.length; i += 1) {
-    if (i % 2 === 0) {
-      sumEven += i;
-    } else {
-      sumOdd += 1;
-    }
-  }
-  return [sumEven, sumOdd];
-}
+// function sumEvenAndOdd(array) {
+//   let sumEven = 0;
+//   let sumOdd = 0;
+//   for (let i = 1; i <= array.length; i += 1) {
+//     if (i % 2 === 0) {
+//       sumEven += i;
+//     } else {
+//       sumOdd += 1;
+//     }
+//   }
+//   return [sumEven, sumOdd];
+// }
 
-//3. У вас є масив з мов кодування. Кожен елемент масиву - це рядок з назвою мови та її скороченням у форматі "назва скорочення" (наприклад, "JavaScript JS"). Ваша мета - перетворити кожен елемент масиву у формат "назва мови: скорочення" та записати їх у новий масив.
+// 3. У вас є масив з мов кодування. Кожен елемент масиву - це рядок з назвою мови та її скороченням у форматі "назва скорочення" (наприклад, "JavaScript JS"). Ваша мета - перетворити кожен елемент масиву у формат "назва мови: скорочення" та записати їх у новий масив.
 // const codingLanguages = [
 //   "JavaScript JS",
 //   "Python PY",
@@ -49,8 +49,21 @@ function sumEvenAndOdd(array) {
 //   "Java JV",
 //   "C# CS",
 // ];
+
+
+// function formatLanguages(codingLanguages){
+//   const result = []
+
+//   for (const iterator of codingLanguages) {
+//     const arrIterator = iterator.split(' ').join(': ')
+    
+//     result.push(arrIterator)
+//   }
+// return result
+// }
+
 // const result  = formatLanguages(codingLanguages)
-//console.log(result); // ["JavaScript: JS", "Python: PY", "Ruby: RB", "Java: JV", "C#: CS"];
+// console.log(formatLanguages(codingLanguages)); // ["JavaScript: JS", "Python: PY", "Ruby: RB", "Java: JV", "C#: CS"];
 
 // 4. Написати функцію, яка приймає число n та повертає n-те число Фібоначчі.
 
