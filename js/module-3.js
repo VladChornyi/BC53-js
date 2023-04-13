22250b2b5311d1b3cdcaf455d12944f57d49cb26
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-04-13 14:30:27 +0300

    3модуль

--- js/module-3.js
+++ js/module-3.js
@@ -4,7 +4,8 @@
 //4. Створіть об'єкт "кошик" зі списком товарів та їх цін. Напишіть метод "calculateTotal", який буде обчислювати загальну суму товарів у кошику.
 //5. Створіть об'єкт "автомобіль" з властивостями "марка", "модель" та "швидкість". Напишіть метод "accelerate", який буде збільшувати швидкість автомобіля на 10 км/год.
 
-//6. Створіть масив об'єктів "студентів" з властивостями "ім'я", "прізвище" та "середній бал". Напишіть функцію "findTopStudent", яка буде повертати об'єкт студента з найвищим середнім балом.
+//6. Створіть масив об'єктів "студентів" з властивостями "ім'я", "прізвище" та "середній бал".
+// Напишіть функцію "findTopStudent", яка буде повертати об'єкт студента з найвищим середнім балом.
 // const students = [
 //   { name: "Андрій", surname: "Іванов", grade: 4.5 },
 //   { name: "Олександр", surname: "Петров", grade: 3.9 },
@@ -12,7 +13,21 @@
 //   { name: "Ірина", surname: "Федорова", grade: 4.2 },
 // ];
 
-//7. Створіть масив об'єктів "книг" з властивостями "назва", "автор" та "рік видання". Напишіть функцію "findBooksByAuthor", яка буде повертати масив книг відповідного автора.
+// const findTopStudent = (studArray) => {
+//   let best_student = studArray[0];
+//   for (const student of studArray) {
+//     //console.log(student);
+//     if (student.grade > best_student.grade) {
+//       best_student = student;
+//     }
+//   }
+//   return best_student;
+// };
+// const stud = findTopStudent(students);
+// console.log(stud);
+
+//7. Створіть масив об'єктів "книг" з властивостями "назва", "автор" та "рік видання". Напишіть функцію "findBooksByAuthor",
+//яка буде повертати масив книг відповідного автора.
 // const books = [
 //   { title: "Війна і мир", author: "Лев Толстой", year: 1869 },
 //   {
@@ -29,7 +44,19 @@
 //   { title: "1984", author: "Джордж Оруелл", year: 1949 },
 // ];
 
-//8. Створіть масив об'єктів "продуктів" з властивостями "назва", "ціна" та "кількість". Напишіть функцію "calculateTotalPrice", яка буде повертати загальну вартість всіх товарів в масиві.
+// const findBooksByAuthor = (bokksArray, author) => {
+//   const booksAuthor = [];
+//   for (const book of bokksArray) {
+//     book.author === author && booksAuthor.push(book.title);
+//   }
+//   return booksAuthor;
+// };
+
+// const arr = findBooksByAuthor(books, "Джоан Роулінг");
+// console.log(arr);
+
+//8. Створіть масив об'єктів "продуктів" з властивостями "назва", "ціна" та "кількість".
+// Напишіть функцію "calculateTotalPrice", яка буде повертати загальну вартість всіх товарів в масиві.
 
 // const products = [
 //   { name: "Молоко", price: 22, quantity: 3 },
@@ -37,3 +64,13 @@
 //   { name: "Сир", price: 120, quantity: 1 },
 //   { name: "Яблука", price: 18, quantity: 5 },
 // ];
+
+// const calculateTotalPrice = (productsArray) => {
+//   let totalPrice = 0;
+//   for (const product of productsArray) {
+//     totalPrice += product.price * product.quantity;
+//   }
+//   return totalPrice;
+// };
+// const total = calculateTotalPrice(products);
+// console.log(`Всього: ${total}`);
