ac3ca9a7a0d04f051c5ed46d816d139b48554310
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-04-18 16:50:51 +0300

    8 total

--- js/additionalPractice-2.js
+++ js/additionalPractice-2.js
@@ -48,12 +48,256 @@
 //   { name: 'Mike', age: 40, gender: 'male' }
 // ];
 // Потрібно отримати список імен жінок старше 25 років, відсортований за алфавітом.
+const orders = [
+  {
+    id: 1,
+    name: "John",
+    date: "2022-01-01",
+    total: 50,
+    paid: true,
+    items: [
+      { id: 1, name: "book", quantity: 2 },
+      { id: 2, name: "pen", quantity: 5 },
+    ],
+  },
+  {
+    id: 2,
+    name: "Jane",
+    date: "2022-02-03",
+    total: 20,
+    paid: true,
+    items: [
+      { id: 1, name: "book", quantity: 1 },
+      { id: 3, name: "pencil", quantity: 3 },
+    ],
+  },
+  {
+    id: 3,
+    name: "Bob",
+    date: "2022-03-05",
+    total: 100,
+    paid: false,
+    items: [
+      { id: 2, name: "pen", quantity: 10 },
+      { id: 4, name: "notebook", quantity: 1 },
+    ],
+  },
+  {
+    id: 4,
+    name: "Alice",
+    date: "2022-04-07",
+    total: 70,
+    paid: true,
+    items: [
+      { id: 3, name: "pencil", quantity: 4 },
+      { id: 4, name: "notebook", quantity: 2 },
+    ],
+  },
+  {
+    id: 5,
+    name: "Jim",
+    date: "2022-05-09",
+    total: 30,
+    paid: true,
+    items: [
+      { id: 1, name: "book", quantity: 1 },
+      { id: 2, name: "pen", quantity: 2 },
+    ],
+  },
+  {
+    id: 6,
+    name: "Sara",
+    date: "2022-06-11",
+    total: 80,
+    paid: true,
+    items: [
+      { id: 3, name: "pencil", quantity: 6 },
+      { id: 4, name: "notebook", quantity: 3 },
+    ],
+  },
+  {
+    id: 7,
+    name: "John",
+    date: "2022-01-03",
+    total: 90,
+    paid: true,
+    items: [
+      { id: 3, name: "pencil", quantity: 2 },
+      { id: 2, name: "pen", quantity: 5 },
+      { id: 1, name: "book", quantity: 1 },
+    ],
+  },
+  {
+    id: 8,
+    name: "Jim",
+    date: "2022-08-09",
+    total: 30,
+    paid: true,
+    items: [
+      { id: 1, name: "book", quantity: 1 },
+      { id: 4, name: "notebook", quantity: 2 },
+    ],
+  },
+  {
+    id: 9,
+    name: "Jim",
+    date: "2022-06-11",
+    total: 45,
+    paid: true,
+    items: [
+      { id: 3, name: "pencil", quantity: 1 },
+      { id: 2, name: "pen", quantity: 2 },
+    ],
+  },
+  {
+    id: 10,
+    name: "John",
+    date: "2022-08-03",
+    total: 90,
+    paid: true,
+    items: [{ id: 1, name: "book", quantity: 1 }],
+  },
+];
 
-// 8***. Задача: дано масив об'єктів, що представляють замовлення в інтернет-магазині. Кожен об'єкт містить поля з номером замовлення, ім'я клієнта, датою замовлення, сумою замовлення та статусом (заплачено або ні). Потрібно отримати масив об'єктів з інформацією про топ-5 клієнтів, які зробили найбільше замовлень, та середньою сумою їх замовлень, а також масив об'єктів з інформацією про топ-5 товарів за кількістю продажів, та загальну кількість проданих одиниць кожного товару.
-// const orders = [
-//   { id: 1, name: 'John', date: '2022-01-01', total: 50, paid: true, items: [{id: 1, name: 'book', quantity: 2}, {id: 2, name: 'pen', quantity: 5}] },
-//   { id: 2, name: 'Jane', date: '2022-02-03', total: 20, paid: true, items: [{id: 1, name: 'book', quantity: 1}, {id: 3, name: 'pencil', quantity: 3}] },
-//   { id: 3, name: 'Bob', date: '2022-03-05', total: 100, paid: false, items: [{id: 2, name: 'pen', quantity: 10}, {id: 4, name: 'notebook', quantity: 1}] },
-//   { id: 4, name: 'Alice', date: '2022-04-07', total: 70, paid: true, items: [{id: 3, name: 'pencil', quantity: 4}, {id: 4, name: 'notebook', quantity: 2}] },
-//   { id: 5, name: 'Jim', date: '2022-05-09', total: 30, paid: true, items: [{id: 1, name: 'book', quantity: 1}, {id: 2, name: 'pen', quantity: 2}] },
-//   { id: 6, name: 'Sara', date: '2022-06-11', total: 80, paid: true, items: [{id: 3, name: 'pencil', quantity: 6}, {id: 4, name: 'notebook', quantity: 3}] }]
+// 8***. Задача: дано масив об'єктів, що представляють замовлення в інтернет-магазині.
+// Кожен об'єкт містить поля з номером замовлення, ім'я клієнта, датою замовлення, сумою замовлення та
+// статусом(заплачено або ні).Потрібно отримати масив об'єктів з інформацією про топ-5 клієнтів, які
+// зробили найбільше замовлень, та середньою сумою їх замовлень, а також масив об'єктів з інформацією
+//  про топ - 5 товарів за кількістю продажів, та загальну кількість проданих одиниць кожного товару.
+// //
+
+const firstUsers = (array) => {
+  const orders = [];
+
+  for (let i = 0; i < array.length; i += 1) {
+    const order = {};
+    order.items = [];
+    if (order.length === 0) {
+      order.name = array[i].name;
+      order.total = array[i].total;
+      order.items = array[i].items;
+
+      orders.push(order);
+    } else {
+      let userPocupa = false;
+      for (let i = 0; i < orders.length; i += 1) {
+        if (orders.name === array[i].name) {
+          userPocupa = true;
+          orders[i].total += array[i].total;
+          orders[i].items.push(array[i].items);
+        }
+      }
+      if (!userPocupa) {
+        order.name = array[i].name;
+        order.total = array[i].total;
+        order.items = array[i].items;
+
+        orders.push(order);
+      }
+    }
+  }
+
+  return orders.sort((a, b) => b.total - a.total).slice(0, 5);
+};
+
+const result_users = firstUsers(orders);
+console.log(result_users);
+
+// const topUsersClient = (array, number) => {
+//   const topClients = [];
+//   array.map((order) => {
+//     const clientOrder = {
+//       name: "",
+//       total: 0,
+//       data: [],
+//       countPocupoc: 0,
+//       items: [],
+//       average_value: 0,
+//     };
+//     if (topClients.length === 0) {
+//       clientOrder.name = order.name;
+//       clientOrder.total = order.total;
+//       clientOrder.data.push(order.date);
+//       clientOrder.countPocupoc = 1;
+//       clientOrder.items.push(order.items);
+//       clientOrder.average_value = order.total;
+//       topClients.push(clientOrder);
+//     } else {
+//       // проверяем может такой клиент уже есть в лучших клиентах
+//       // если да то мі одаємо данніе в его данніе
+//       let yesClient = false;
+//       for (let i = 0; i < topClients.length; i = i + 1) {
+//         if (topClients[i].name === order.name) {
+//           yesClient = true;
+//           topClients[i].total += order.total;
+//           topClients[i].countPocupoc += 1;
+//           topClients[i].data.push(order.date);
+//           topClients[i].items.push(order.items);
+//           topClients[i].average_value =
+//             topClients[i].total / topClients[i].countPocupoc;
+//         }
+//         continue;
+//       }
+
+//       //если у нас небуло ще такого кліента то ми його додаємоу topClients
+//       if (!yesClient) {
+//         clientOrder.name = order.name;
+//         clientOrder.total = order.total;
+//         clientOrder.data.push(order.date);
+//         clientOrder.countPocupoc = 1;
+//         clientOrder.items.push(order.items);
+//         clientOrder.average_value =
+//           clientOrder.total / clientOrder.countPocupoc;
+
+//         topClients.push(clientOrder);
+//       }
+//     }
+//   });
+//   topClients.sort((a, b) => b.total - a.total); // cortirovca по количеству витрачених коштів
+
+//   return topClients;
+// };
+
+// const topProducts = (array) => {
+//   let products;
+
+//   products = array.flatMap((order) => order.items);
+//   const firstProducts = [];
+//   products.map((product) => {
+//     const tovar = {};
+//     if (firstProducts.length === 0) {
+//       tovar.id = product.id;
+//       tovar.name = product.name;
+//       tovar.quantity = product.quantity;
+//       tovar.countProdagiv = 1;
+//       firstProducts.push(tovar);
+//     } else {
+//       //проверяем может у нас уже есть такой товар в массиве
+//       let yesTovar = false;
+//       for (let i = 0; i < firstProducts.length; i += 1) {
+//         if (firstProducts[i].id === product.id) {
+//           yesTovar = true;
+//           firstProducts[i].quantity += product.quantity;
+//           firstProducts[i].countProdagiv += 1;
+//         }
+//         continue;
+//       }
+
+//       if (!yesTovar) {
+//         //додаємо новий товар
+//         tovar.id = product.id;
+//         tovar.name = product.name;
+//         tovar.quantity = product.quantity;
+//         tovar.countProdagiv = 1;
+//         firstProducts.push(tovar);
+//       }
+//     }
+//   });
+//   return firstProducts.sort((a, b) => b.quantity - a.quantity);
+// };
+// const resultUsers = topUsersClient(orders);
+// console.log(resultUsers);
+
+// const productsFavorit = topProducts(orders);
+
+// console.log(productsFavorit);
