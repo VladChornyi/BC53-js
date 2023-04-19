53bd88a939fac7159cce612ba5142b0b1a37bf6d
ginkosoul <ginkosoul@gmail.com>
2023-04-19 15:45:56 +0300

    solved

--- js/module-5.js
+++ js/module-5.js
@@ -4,61 +4,102 @@
 //   lastName: 'Doe',
 //   age: 30,
 //   email: 'johndoe@example.com',
-//   info: ???
+//   info(){
+//     console.log(`Ім'я: ${this.firstName} Прізвище: ${this.lastName} Вік: ${this.age} Email: ${this.email}`)
+//   }
 // }
+
 
 // user.info();
 
 // 2. Написати метод для зміни ім'я або прізвища(змінити можна лише якесь з цих полів, змінювати або додавати нові потрібно заборонити) з перевіркою на валідність даних(Перша літера має бути у верхньому реєстрі, довжина слова не менше 3 літер)
+// function validate(str){
+//     if (str)
+//     if ((str[0] === str[0].toUpperCase()) && (str.length > 2)){
+//         return str;
+//     }
+//     return false;
+// }
+// function changeFirstName(oldName, newName){
+//     if (!validate(newName)) return 'Enter valid name'
 
+//     if (this.firstName === oldName) {
+//         this.firstName = newName;
+//         return `${oldName} changed to ${newName}`
+//     }
+// }
+
+// console.log(changeFirstName.call(user,'John','Vlad'))
 // 3. Створити глобальну функцію, що може додавати властивості об'єкту, в контексті якого вона буде викликана. Додати цю функцію як метод об'єкта user, та додати за допомогою неї властивість friends із значенням:
-// [
-// {
-//     firstName: 'Mary',
-//     lastName: 'Smith',
-//     age: 32,
-//     email: 'marysmith@hotmail.com'
-//   },
-//   {
-//     firstName: 'Alex',
-//     lastName: 'Johnson',
-//     age: 45,
-//     email: 'alex.johnson@yahoo.com'
-//   },
-//   {
-//     firstName: 'Emily',
-//     lastName: 'Davis',
-//     age: 19,
-//     email: 'emilydavis@gmail.com'
-//   }]
+const users = [
+{
+    firstName: 'Mary',
+    lastName: 'Smith',
+    age: 32,
+    email: 'marysmith@hotmail.com'
+  },
+  {
+    firstName: 'Alex',
+    lastName: 'Johnson',
+    age: 45,
+    email: 'alex.johnson@yahoo.com'
+  },
+  {
+    firstName: 'Emily',
+    lastName: 'Davis',
+    age: 19,
+    email: 'emilydavis@gmail.com'
+  }]
+
+  function newField(name, value){
+    this[name] = value;
+    return `added ${name}: ${this[name]} to ${this.firstName}`
+  }
+  function makeMagic(arr, name, value){
+    for (const obj of arr) {
+        // newField.bind(obj);
+        // newField(name, value);
+        console.log(newField.call(obj, name, value));
+    }
+  }
+
+  makeMagic(users,`friends`, [`Red`, `Grey`, `Marry`]);
+  console.log(users);
 
 // 4. Викликати метод user.info() таким чином, щоб він вивів результатом  ({name: 'Bob', lactName: 'Lasso',age: 50, email: 'BodLasso@gmail.com'})
+// user.info.call({name: 'Bob', lactName: 'Lasso',age: 50, email: 'BodLasso@gmail.com'});
 // 5. створіть об'єкт "Кошик", який буде мати список продуктів та методи для додавання і видалення продуктів. Також створіть об'єкт "Замовлення", який буде мати метод для отримання списку продуктів з кошика та оформлення замовлення. Зв'яжіть метод оформлення замовлення з методом "Оформити замовлення" об'єкта "Кошик" за допомогою bind.
 
 // const cart = {
 //   products: [],
 
 //   addProduct(product) {
-//
+//     this.products.push(product)
 //     console.log(`Product ${product} added to cart`);
 //   },
 
 //   removeProduct(product) {
-//
+//     const index = this.products.find(e => e === product)
+//     return index === -1 ? `${product} does not exist` : `${this.products.splice(index, 1)} removed successful`
 //   },
 
 //   checkout() {
 //     console.log(`Ordering products: ${this.products.join(", ")}`);
+//     return this.products.join(", ");
 //   }
 // };
 
 // const order = {
-//   checkoutCart: null,
+//   checkoutCart(){
+//     // return cart.checkout();
+//     // return checkout.call(cart);
+//     return this.products.join(", ");
+//   },
 
 //   placeOrder() {
 //     if (this.checkoutCart) {
 //       console.log("Placing order...");
-//       this.checkoutCart();
+//       this.checkoutCart.call(cart);
 //       console.log("Order placed successfully!");
 //     } else {
 //       console.log("No items in cart to order");
@@ -71,15 +112,15 @@
 // cart.addProduct("Banana");
 
 // order.placeOrder();
-// Ordering products: Apple, Orange, Banana
-// Placing order...
-// Ordering products: Apple, Orange, Banana
-// Order placed successfully!
+// // Ordering products: Apple, Orange, Banana
+// // Placing order...
+// // Ordering products: Apple, Orange, Banana
+// // Order placed successfully!
 
 // cart.removeProduct("Orange");
 
 // order.placeOrder();
-// Ordering products: Apple, Banana
-// Placing order...
-// Ordering products: Apple, Banana
-// Order placed successfully!
+// // Ordering products: Apple, Banana
+// // Placing order...
+// // Ordering products: Apple, Banana
+// // Order placed successfully!
