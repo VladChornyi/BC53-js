87d6f646165ed4c840dd3ef56c4c5a6b00780f0b
ilkash <126604951+ilkash@users.noreply.github.com>
2023-04-21 15:08:42 +0300

    6-9

--- js/module-6.js
+++ js/module-6.js
@@ -1,17 +1,27 @@
-const body = document.querySelector('body')
+ const body = document.querySelector('body')
 
 
-const title = document.getElementById('title')
+const
+    title = document.getElementById('title')
 
-const list = document.querySelector('.list')
 
- const dataTopic = document.querySelectorAll('[data-topic]')
- dataTopic.forEach((el)=> console.log(el)) 
+ const list = document.querySelector('.list')
 
- const forEach = (callBack)=>{
-    const index = 0;
-    const element = "<div></div>";
-    const array = ["<div></div>","<div></div>","<div></div>"]
-callBack(element, index, array)
- } 
- 
+//  const dataTopic = document.querySelectorAll('[data-topic]')
+//  dataTopic.forEach((el)=> console.log(el)) 
+
+//  const forEach = (callBack)=>{
+//     const index = 0;
+//     const element = "<div></div>";
+//     const array = ["<div></div>","<div></div>","<div></div>"]
+// callBack(element, index, array)
+//  } 
+ const dataTopicEl = document.querySelector('[data-topic]')
+console.log(dataTopicEl)
+
+const dataTopic = document.querySelectorAll('[data-topic]')
+//console.log(dataTopic[dataTopic.length - 1])
+const a = title.nextElementSibling
+// console.log(a)
+const subtitles = document.querySelectorAll('h3')
+ subtitles.forEach((el)=> el.classList.add("active")) 
