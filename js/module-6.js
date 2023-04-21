a2f67b3f53c288cf63be401a196eeb0d182d084e
HetmanKateryna0107 <125737708+HetmanKateryna0107@users.noreply.github.com>
2023-04-21 14:39:42 +0300

    1

---
+++ js/module-6.js
@@ -0,0 +1,17 @@
+const body = document.querySelector('body')
+
+
+const title = document.getElementById('title')
+
+const list = document.querySelector('.list')
+
+ const dataTopic = document.querySelectorAll('[data-topic]')
+ dataTopic.forEach((el)=> console.log(el)) 
+
+ const forEach = (callBack)=>{
+    const index = 0;
+    const element = "<div></div>";
+    const array = ["<div></div>","<div></div>","<div></div>"]
+callBack(element, index, array)
+ } 
+ 
