abf1843b16dedeb00888836748eca1104aadbbcf
Vladyslav <vladyslavromanenkoo@gmail.com>
2023-04-21 16:06:18 +0300

    practice

--- js/module-6.js
+++ js/module-6.js
@@ -1,8 +1,8 @@
-const body = document.querySelector('body');
+const body = document.querySelector("body");
 
-const title = document.getElementById('title');
+const title = document.getElementById("title");
 
-const list = document.querySelector('.list');
+const list = document.querySelector(".list");
 
 // const dataTopic = document.querySelectorAll('[data-topic]');
 // dataTopic.forEach(el => console.log(el));
@@ -27,6 +27,31 @@ const list = document.querySelector('.list');
 // console.log(navigation);
 // navigation.style.backgroundColor = 'yellow';
 // navigation.lastChild.textContent = 'Я змінив тут текст!';
-const currentTopic = 'manipulation';
+const currentTopic = "manipulation";
 const findElement = document.querySelector(`[data-topic=${currentTopic}]`);
-console.log(findElement);
+// console.log(findElement);
+findElement.style.backgroundColor = "blue";
+const completed = document.querySelector(".completed");
+// console.log(completed);
+
+completed.parentNode.remove();
+// console.log(completed);
+const par = document.createElement("p");
+par.textContent = "Об'єктна модель документа (Document Object Model)";
+title.after(par);
+
+// const supTitle = document.createElement("h3");
+// supTitle.textContent = "Властивість innerHTML";
+// const parag = document.createElement("p");
+// parag.textContent =
+//   "Ще один спосіб створити DOM-елементи і помістити їх в дерево - це використовувати рядки з тегами і дозволити браузеру зробити всю важку роботу";
+// const listItem = document.createElement("li");
+// listItem.append(supTitle, parag);
+
+// list.append(listItem);
+
+const markup =
+  "<li> <h3>Властивість innerHTML</h3> <p>Ще один спосіб створити DOM-елементи і помістити їх в дерево - це використовувати рядки з тегами і дозволити браузеру зробити всю важку роботу</p></li>";
+list.insertAdjacentHTML("beforeend", markup);
+
+list.innerHTML = "";
