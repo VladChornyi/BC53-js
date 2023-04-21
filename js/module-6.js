f722bf976a2f6825e110cc2761bfca5c0de69e09
getchagrooveon <eugene.osipov.1989@gmail.com>
2023-04-21 15:35:07 +0300

    ggg

--- js/module-6.js
+++ js/module-6.js
@@ -1,27 +1,32 @@
- const body = document.querySelector('body')
+const body = document.querySelector('body');
 
+const title = document.getElementById('title');
 
-const
-    title = document.getElementById('title')
+const list = document.querySelector('.list');
 
+// const dataTopic = document.querySelectorAll('[data-topic]');
+// dataTopic.forEach(el => console.log(el));
 
- const list = document.querySelector('.list')
+// const forEach = callBack => {
+//   const index = 0;
+//   const element = '<div></div>';
+//   const array = ['<div></div>', '<div></div>', '<div></div>'];
+//   callBack(element, index, array);
+// };
+// const dataTopicEl = document.querySelector('[data-topic]');
+// console.log(dataTopicEl);
 
-//  const dataTopic = document.querySelectorAll('[data-topic]')
-//  dataTopic.forEach((el)=> console.log(el)) 
+// const dataTopic = document.querySelectorAll('[data-topic]');
+// //console.log(dataTopic[dataTopic.length - 1])
+// const a = title.nextElementSibling;
+// // console.log(a)
+// const subtitles = document.querySelectorAll('h3');
+// subtitles.forEach(el => el.classList.add('active'));
 
-//  const forEach = (callBack)=>{
-//     const index = 0;
-//     const element = "<div></div>";
-//     const array = ["<div></div>","<div></div>","<div></div>"]
-// callBack(element, index, array)
-//  } 
- const dataTopicEl = document.querySelector('[data-topic]')
-console.log(dataTopicEl)
-
-const dataTopic = document.querySelectorAll('[data-topic]')
-//console.log(dataTopic[dataTopic.length - 1])
-const a = title.nextElementSibling
-// console.log(a)
-const subtitles = document.querySelectorAll('h3')
- subtitles.forEach((el)=> el.classList.add("active")) 
+// const navigation = document.querySelector('li[data-topic="navigation"]');
+// console.log(navigation);
+// navigation.style.backgroundColor = 'yellow';
+// navigation.lastChild.textContent = 'Я змінив тут текст!';
+const currentTopic = 'manipulation';
+const findElement = document.querySelector(`[data-topic=${currentTopic}]`);
+console.log(findElement);
