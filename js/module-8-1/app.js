f51f068a32292beb7766730c0d2ea6ca33f7a2e2
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-04-28 15:42:57 +0300

    commit

--- js/module-8-1/app.js
+++ js/module-8-1/app.js
@@ -1,12 +1,32 @@
 import expenses from "./transactions.js";
-import { calculateTotalExpenses } from "./expenses.js";
-import { getExpensesByCategory } from "./expenses.js";
+
+import {
+  getExpensesByCategory,
+  getUnicCategories,
+  renderOptions,
+  calculateTotalExpenses,
+} from "./expenses.js";
+
 // import * as test from "./expenses.js";
+const select = document.querySelector("#expenses");
+const categories = getUnicCategories(expenses);
+
+const count = document.querySelector(".count");
+
+renderOptions(categories, select);
 
 const totalExpenses = calculateTotalExpenses(expenses);
 console.log(`Загальна сума витрат: ${totalExpenses}`);
 
-const foodExpenses = calculateTotalExpenses(
-  getExpensesByCategory(expenses, "Їжа")
+select.addEventListener("change", hahdleChenge);
+
+function hahdleChenge(e) {
+  const countArray = getExpensesByCategory(expenses, e.target.value);
+  const total = calculateTotalExpenses(countArray);
+  count.textContent = total;
+}
+
+count.textContent = calculateTotalExpenses(
+  getExpensesByCategory(expenses, select.value)
 );
-console.log(`Витрати на їжу: ${foodExpenses}`);
+//
--- js/module-8-1/expenses.js
+++ js/module-8-1/expenses.js
@@ -5,3 +5,23 @@ export function calculateTotalExpenses(arr) {
 export function getExpensesByCategory(arr, name) {
   return arr.filter((el) => el.category === name);
 }
+
+export function getUnicCategories(array) {
+  return array
+    .map((item) => {
+      return item.category;
+    })
+    .filter((el, i, arr) => arr.indexOf(el) === i);
+}
+
+export function renderOptions(array, element) {
+  const markup = array
+    .map((item) => {
+      return `
+    <option value="${item}">${item}</option>
+    `;
+    })
+    .join("");
+
+  element.innerHTML = markup;
+}
