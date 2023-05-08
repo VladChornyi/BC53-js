ba171d8fbc03e9de483da224aac20d96bc1d0630
Iryna <irinkasatanovskaya@gmail.com>
2023-05-08 15:19:43 +0300

    Update bc53-students.js

--- js/module-10-1/bc53-students.js
+++ js/module-10-1/bc53-students.js
@@ -1,5 +1,11 @@
 const BASE_URL = "https://63c7e1d3075b3f3a91d50f37.mockapi.io/";
 const studentsUl = document.querySelector(".studentsUl");
+const addBtn = document.querySelector(".add_student");
+
+const student = {
+  name: "Hannah",
+  lastName: "Smith",
+};
 
 const createStudentCard = (el) => {
   return `<li>
@@ -13,7 +19,23 @@ const renderList = (arr) => {
   studentsUl.innerHTML = students;
 };
 
+const renderEl = (el) => {
+  studentsUl.insertAdjacentHTML("beforeend", el);
+};
+
 const getStudentList = () => {
   return fetch(`${BASE_URL}bc53-students`).then((response) => response.json());
 };
 getStudentList().then(renderList);
+
+const addStudent = (student) => {
+  return fetch(`${BASE_URL}bc53-students`, {
+    headers: { "Content-Type": "application/json" },
+    method: "POST",
+    body: JSON.stringify(student),
+  }).then((response) => response.json());
+};
+
+addBtn.addEventListener("click", () => {
+  addStudent(student).then(createStudentCard).then(renderEl);
+});
