88d6441d5e270b562c29ab23ba1c1b7ca1a6a814
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-05-10 16:28:19 +0300

    commit" [Window Title] Visual Studio Code
    [Main Instruction]
    Are you sure you want to paste 3 lines of text into the terminal?
    
    [Content]
    Preview:
    axios.create({
      baseURL: https://rickandmor…

--- src/index.js
+++ src/index.js
@@ -1,21 +1,47 @@
-import {getAllCharacters} from './rickAndMorty'
+import { getAllCharacters, getByParams } from './rickAndMorty';
 
-const ul = document.querySelector('ul')
-async function renderCharacters (){
-    // console.log(await getAllCharacters()) 
-   const dataCharacters = await getAllCharacters()
+const ul = document.querySelector('ul');
 
-   const result = dataCharacters.map (element => createCard(element))
-    ul.insertAdjacentHTML('beforeend', result.join(''))
+const form = document.querySelector('form');
+
+form.addEventListener('submit', handleSibmit);
+
+async function getData() {
+  try {
+    const dataCharacters = await getAllCharacters();
+    renderCharacters(dataCharacters);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-renderCharacters ()
+getData();
 
-function createCard (data){
-   return `
+async function renderCharacters(data) {
+  const result = data.map(element => createCard(element));
+  ul.innerHTML = result.join('');
+}
+
+async function handleSibmit(e) {
+  e.preventDefault();
+  const param = {
+    name: e.target.elements.name.value,
+    status: e.target.elements.status.value,
+    gender: e.target.elements.gender.value,
+  };
+  try {
+    const data = await getByParams(param);
+    renderCharacters(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function createCard(data) {
+  return `
    <li>
         <img src='${data.image}' width='100'>
         <p class='name'>${data.name}</p>
    </li>
-   `
+   `;
 }
--- src/rickAndMorty.js
+++ src/rickAndMorty.js
@@ -1,10 +1,30 @@
 // https://rickandmortyapi.com/
-import axios from 'axios'
-axios.defaults.baseURL = "https://rickandmortyapi.com/api/"
+import axios from 'axios';
+axios.defaults.baseURL = 'https://rickandmortyapi.com/api/';
 
-export const getAllCharacters = async () =>{
-                                                               // axios.get('character').then({data}=>data.results)
-    const {data} = await axios.get('character')
-    return data.results    
-}
+const API = axios.create({
+  baseURL: 'https://rickandmortyapi.com/api/',
+});
+// const ImageAPI = axios.create({
+//   baseURL: 'https://rickandmortyapi.com/api/',
+// });
 
+export const getAllCharacters = async () => {
+  // axios.get('character').then({data}=>data.results)
+  const { data } = await API.get('character');
+  return data.results;
+};
+
+export const getByParams = async params => {
+  const { data } = await API.get('character', {
+    params,
+  });
+  return data.results;
+};
+
+//character/?name=rick&status=alive
+// getByParams({
+//   name: 'Rick',
+//   status: 'alive',
+//   gender: 'Male',
+// });
