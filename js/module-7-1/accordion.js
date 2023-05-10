5fc051a4fe05ce85d9d02103d02e16015b1493d3
Vlad <4ornii.vlad@gmail.com>
2023-05-10 13:44:58 +0300

    add parcel

--- js/module-7-1/accordion.js
+++
@@ -1,9 +0,0 @@
-const listRef = document.querySelector('.js-accordion-list');
-
-function handleBtnClick(event) {
-  if (event.target.classList.contains('js-accordion-btn')) {
-    event.target.nextElementSibling.classList.toggle('active');
-  }
-}
-
-listRef.addEventListener('click', handleBtnClick);
